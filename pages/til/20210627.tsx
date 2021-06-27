8de385617d9c703057c34d0f82084e5c9b3dca7f
31 <monsterfairy2@gmail.com>
2021-06-27 16:14:41 +0900

    27d일 내용 추가

--- pages/til/20210627.tsx
+++ pages/til/20210627.tsx
@@ -12,7 +12,7 @@ const FirstPage = () => {
     <Post
       title="JS"
       refList={[
-        'https://kimchanjung.github.io/programming/2020/07/05/typescipt-import-js-module-error/',
+        'https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/',
       ]}
     >
       <>
@@ -40,12 +40,18 @@ let test2 = 'test2'; //Uncaught SyntaxError: Identifier 'test2' has already been
             }}
           />
           3. hoisting var 로 선언한 변수는 스코프의 최상단으로 선언부가
-          끌어올려지게 되고, undefined로 초기화 된다.
+          끌어올려지게 되고, undefined로 초기화까지 되지만 , let, const는
+          scope의 top위치로 hoisting되지만 초기화 되지 않아서 초기화 시점 이전에
+          변수를 사용할 경우 Reference Error가 발생한다.
           <Editor
             value={`
 console.log(test); // undefined
 var test= 1;
 console.log(test); //1
+
+console.log(test2) // Reference Error.
+let test2 = 1;
+console.log(test2);
 `}
             onValueChange={(value) => console.log(value)}
             highlight={(value) => highlight(value, languages.js)}
@@ -57,18 +63,47 @@ console.log(test); //1
           />
         </p>
 
-        <Editor
-          value={`
-.selector {word-break:break-all;}
+        <p>
+          4. let과 const은 둘 다 재 선언이 불가능 하지만 let은 새로운 값으로
+          update가 가능하고, const는 선언 시점에 초기화 해야 하고, 그 이후에는
+          업데이트가 불가능하다.
+          <Editor
+            value={`
+let letTest = '1';
+letTest = '2';
+let letTest = '3'; // error: Identifier 'letTest' has already been declared
+
+const constTest = '1';
+constTest = '2'; // error: Assignment to constant variable. 
+const constTest = '3'; // error: Identifier 'constTest' has already been declared
+
+const constObj = {name: 'test'}; //const 로 Object형태로 선언할 경우 properties는 에러 없이 업데이트 가능하다.
+constObj.name = 'value';
 `}
-          onValueChange={(value) => console.log(value)}
-          highlight={(value) => highlight(value, languages.js)}
-          padding={10}
-          style={{
-            fontFamily: '"Fira code", "Fira Mono", monospace',
-            fontSize: 12,
-          }}
-        />
+            onValueChange={(value) => console.log(value)}
+            highlight={(value) => highlight(value, languages.js)}
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 12,
+            }}
+          />
+        </p>
+
+        <br />
+        <p>
+          전체 요약
+          <br />
+          1.var는 function scope, let, const는 block scope <br />
+          2.var변수는 수정과 재선언이 둘다 가능하고, let은 수정은 가능, 재선언은
+          불가능, const는 수정과 재선언이 둘다 불가능 하다.
+          <br />
+          3. 3가지 키워드 모두 scope의 상단으로 hoist되고, var은 undefined로
+          초기화 되지만, let과 const는초기화 되지 않는다.
+          <br />
+          4.var과 let은 초기값 없이 선언이 가능하고, const는 선언시점에 초기화
+          되어야 한다.
+        </p>
       </>
     </Post>
   );
